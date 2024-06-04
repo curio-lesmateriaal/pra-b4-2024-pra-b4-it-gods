63137b67eadd8cca0cd315b30475efa99baf7e26
Jesboy3 <D304279@edu.curio.nl>
2024-06-04 11:45:16 +0200

    Revert "pic display naar 2 x 2"
    This reverts commit cd9828d77d810256011e8e14e5346233db6f7f1b.

--- PRA_B4_FOTOKIOSK/magie/PictureManager.cs
+++ PRA_B4_FOTOKIOSK/magie/PictureManager.cs
@@ -13,69 +13,29 @@ namespace PRA_B4_FOTOKIOSK.magie
 {
     public class PictureManager
     {
+
         public static Home Instance { get; set; }
+       
 
         public static void UpdatePictures(List<KioskPhoto> PicturesToDisplay)
         {
             Instance.spPictures.Children.Clear();
-
-            // Group photos into pairs
-            List<List<KioskPhoto>> photoPairs = GroupPhotosIntoPairs(PicturesToDisplay);
-
-            // Display each pair of photos side by side
-            foreach (var pair in photoPairs)
-            {
-                DisplayPhotoPair(pair);
-            }
-        }
-
-        // Group photos into pairs
-        private static List<List<KioskPhoto>> GroupPhotosIntoPairs(List<KioskPhoto> photos)
-        {
-            List<List<KioskPhoto>> photoPairs = new List<List<KioskPhoto>>();
-            List<KioskPhoto> currentPair = new List<KioskPhoto>();
-
-            foreach (var photo in photos)
-            {
-                currentPair.Add(photo);
-
-                // If current pair is complete (contains two photos), add it to the list of pairs and reset the current pair
-                if (currentPair.Count == 2)
+                foreach (KioskPhoto picture in PicturesToDisplay)
                 {
-                    photoPairs.Add(currentPair);
-                    currentPair = new List<KioskPhoto>();
+                    Image image = new Image();
+                    var bitmap = pathToImage(picture.Source);
+                    image.Source = bitmap;
+                    image.Width = 1920 / 3.5;
+                    image.Height = 1080 / 3.5;
+                Instance.spPictures.Children.Add(image);
+                    Console.WriteLine(picture);
                 }
-            }
-
-          
-
-            return photoPairs;
         }
 
-        // Display a pair of photos side by side
-        private static void DisplayPhotoPair(List<KioskPhoto> pair)
-        {
-            StackPanel stackPanel = new StackPanel();
-            stackPanel.Orientation = Orientation.Horizontal;
-
-            foreach (var photo in pair)
-            {
-                Image image = new Image();
-                BitmapImage bitmap = PathToImage(photo.Source);
-                image.Source = bitmap;
-                image.Width = 1920 / 3.5;
-                image.Height = 1080 / 3.5;
-                stackPanel.Children.Add(image);
-            }
-
-            Instance.spPictures.Children.Add(stackPanel);
-        }
-
-        // Convert file path to BitmapImage
-        private static BitmapImage PathToImage(string path)
+        public static BitmapImage pathToImage(string path)
         {
             var stream = new MemoryStream(File.ReadAllBytes(path));
-            var img = new BitmapImage();
+            var img = new System.Windows.Media.Imaging.BitmapImage();
 
             img.BeginInit();
             img.StreamSource = stream;
@@ -83,5 +43,6 @@ namespace PRA_B4_FOTOKIOSK.magie
 
             return img;
         }
+
     }
 }
